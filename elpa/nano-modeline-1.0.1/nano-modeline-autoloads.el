d3356699d84af5eff2dfa17ab429745835f01c2e
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2024-05-06 21:36:18 +0300

    deleted elpa and requests from the chache

--- elpa/nano-modeline-1.0.1/nano-modeline-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; nano-modeline-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "nano-modeline" "nano-modeline.el" (0 0 0 0))
-;;; Generated autoloads from nano-modeline.el
-
-(register-definition-prefixes "nano-modeline" '("nano-modeline-"))
-
-;;;***
-
-;;;### (autoloads nil nil ("nano-modeline-pkg.el") (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; nano-modeline-autoloads.el ends here
