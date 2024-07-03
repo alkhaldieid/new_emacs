929253a7c38b9ff1aa533654c1a4af7c6fbdc9cf
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2024-07-03 14:10:00 +0300

    routine from Laptop

--- init.el
+++ init.el
@@ -38,8 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/repos/org/agenda/monthly.org"
-     "/home/alkhaldieid/repos/org/agenda/jobs.org")))
+   '("~/repos/org/roam/20240612131130-ibm_data_science.org" "/home/alkhaldieid/repos/org/agenda/monthly.org" "/home/alkhaldieid/repos/org/agenda/jobs.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
