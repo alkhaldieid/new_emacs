2bd03f121c9e3989f78a93abeb49d62ed1f3f58a
Eid Alkhaldi <eid.alkhaldi@gmail.com>
2024-11-16 09:39:26 +0300

    working fine

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/repos/org/roam/20240612131130-ibm_data_science.org" "/home/alkhaldieid/repos/org/agenda/monthly.org" "/home/alkhaldieid/repos/org/agenda/jobs.org")))
+   '("/home/alkhaldieid/repos/org/agenda/jobs.org" "/home/alkhaldieid/repos/org/agenda/home.org" "/home/alkhaldieid/repos/org/agenda/work.org" "/home/alkhaldieid/repos/org/roam/20240612131130-ibm_data_science.org" "/home/alkhaldieid/repos/org/agenda/monthly.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
